123ff5b3d1a6956c9a0876ac66347cc9e1e1476f
itstc <itsthomaschu@gmail.com>
2021-01-19 17:46:13 -0700

    added client docker

--- client/src/endpoints.js
+++ client/src/endpoints.js
@@ -1,5 +1,4 @@
-const ENDPOINT_URL = `${process.env.BACKEND_URL || "http://localhost"}:${process.env.BACKEND_PORT || "3000"}`;
-export const GET_ITEMS = `${process.env.MODE === "PROD" ? ENDPOINT_URL : "/api"}/items`; 
+export const GET_ITEMS = "/api/items"; 
 
 export default {
   GET_ITEMS
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,44 +4,51 @@ const webpack = require('webpack'),
 
 const fileExtensions = ["jpg", "jpeg", "png", "gif", "eot", "otf", "svg", "ttf", "woff", "woff2"];
 
-module.exports = {
-  entry: path.join(__dirname, "src", "index.js"),
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: new RegExp('.(' + fileExtensions.join('|') + ')$'),
-        loader: "file-loader?name=[name].[ext]",
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "index.html"),
-      filename: "index.html"
-    })
-  ],
-  devServer: {
+module.exports = () => {
+
+  const plugins = [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, "src", "index.html"),
+        filename: "index.html"
+      })
+  ];
+
+  const devServer = {
     hot: true,
     historyApiFallback: true,
-    port: 8080,
+    port: process.env.CLIENT_PORT || 8080,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: `${process.env.BACKEND_URL}:${process.env.BACKEND_PORT}`,
         pathRewrite: {'^/api': ''}
       }
     }
+  }
+
+  return {
+    entry: path.join(__dirname, "src", "index.js"),
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: new RegExp('.(' + fileExtensions.join('|') + ')$'),
+          loader: "file-loader?name=[name].[ext]",
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    },
+    plugins: plugins,
+    devServer: devServer
   }
 };
